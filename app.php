9feeec127d360778ff35610d26cc5c55b0edb036
Koonoiti <koonoiti@gmail.com>
2026-03-05 18:11:07 +0300

    поредачила app.php вроде даж чот поняла

--- app.php
+++ app.php
@@ -2,7 +2,31 @@
 use App\Repository\UserRepository;
 use App\Service\UserService;
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php'; //вроде не сложно
 
 $repository = new UserRepository();
 $service = new UserService($repository);
+
+$command = $argv[1] ?? null; // сколько не читала в документации пхп ни чёрта не поняла, как это работает и уе8ала у гпт строку
+
+$id = $argv[2] ?? null; // тоже из гпт, я ничо не поняла.
+
+if ($command === 'list') {
+    $users = $service->listUsers();// вызов метода юзер сервис
+    if (empty($users)) {
+        echo "Все враги уничтожены.\n";
+    } else {
+        print_r($users);
+    }
+}
+
+
+if ($command === 'add') {
+    $user = $service->createUser(); // по аналогии из репозитори
+    $repository->add($user);
+    echo "Список врагов пополнен\n";
+}
+if ($command === 'delete' && $id) { // сервис
+    $service->removeUser((int)$id);
+    echo "Враг уничтожен\n";
+}
